d84ca116c5ae651eeb90e92aa49ed6e0cf4adb40
Mohaelm02 <190858298+Mohaelm02@users.noreply.github.com>
2025-06-22 23:19:44 +0200

    modif

--- src/features.h
+++ src/features.h
@@ -8,7 +8,6 @@ void color_green(const char *filename);
 void color_blue(const char *filename);
 void color_gray(const char *filename);
 void color_invert(const char *filename);
-
 void dimension (char *source_path);
 
 void first_pixel (char *source_path);
@@ -25,11 +24,4 @@ void min_component(char *source_path, char component);
 
 void stat_report(char *source_path);
 void color_grey_luminance (char *chemin_image);
-
-void mirror_total(const char *filename);
-void crop_and_resize(const char* filepath, int center_x, int center_y, int target_width, int target_height);
-void scale_nearest(char* filename, float scale_ratio);
-void scale_bilinear(const char *filename, float scale);
-
-
 #endif
